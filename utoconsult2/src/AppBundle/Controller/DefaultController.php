9c95f31a4874db5cedeaaa91a15877cb9911a730
qin cheng <bryant.qin@gmail.com>
2017-09-20 14:17:15 +0200

    front index articles 20170920

--- utoconsult2/src/AppBundle/Controller/DefaultController.php
+++ utoconsult2/src/AppBundle/Controller/DefaultController.php
@@ -15,6 +15,8 @@ class DefaultController extends FrontBaseController
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
+		$articles = $this->container->get('app_utoconsult.ArticleService')->findHomeArticlesByCategory();
+		
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
---
+++ utoconsult2/src/AppBundle/Services/ArticleService.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace AppBundle\Services;
+
+use AppBundle\Entity\UtoconsultArticleCategory1;
+use AppBundle\Entity\UtoconsultArticleCategory2;
+use AppBundle\Services\CacheService;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Doctrine\Bundle\DoctrineBundle\Registry;
+
+class ArticleService {
+    /**
+     * @var Registry
+     */
+    protected $doctrine;
+
+    /*
+     * @var em
+     */
+    protected $em;
+    
+    
+    
+    protected $session;
+
+
+
+    protected $cacheService;
+    
+
+
+
+    public function __construct(Registry $doctrine, Session $session,CacheService $cacheService) 
+    {
+        $this->doctrine = $doctrine;
+        $this->em = $this->doctrine->getManager();
+        $this->session = $session;
+        $this->cacheService = $cacheService;
+    }
+	
+	public function findHomeArticlesByCategory()
+	{
+		$categories1 = $this->em->getRepository('AppBundle:UtoconsultArticleCategory1')->findAllCategory1();
+		$articles = array();
+		foreach($categories1 as $category1){
+			$articles[$category1->getId()]['name'] = $category1->getName();
+			$articles[$category1->getId()]['data'] = $this->em->getRepository('AppBundle:UtoconsultMyArticle')->findArticlesByCategoryId($category1->getId());
+		}
+		return $articles;
+	}
+}
